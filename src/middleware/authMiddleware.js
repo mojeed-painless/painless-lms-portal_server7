b6fd25cff97de24dd47e5591ee2923adee661989
mojeed-painless <shittumjd00@gmail.com>
2026-04-09 00:17:15 +0100

    updated

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -26,7 +26,11 @@ const protect = asyncHandler(async (req, res, next) => {
     } catch (error) {
       console.error(error);
       res.status(401);
-      throw new Error('Not authorized, token failed');
+      if (error.name === 'TokenExpiredError') {
+        throw new Error('Token expired, please login again');
+      } else {
+        throw new Error('Not authorized, token failed');
+      }
     }
   }
 
